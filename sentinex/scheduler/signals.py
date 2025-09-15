4ddcf471b0e178a5942432269982b30b742842be
StudentFPW <jofeleonids00@gmail.com>
2025-09-15 23:30:50 +0300

    added celery configuration

--- sentinex/scheduler/signals.py
+++ sentinex/scheduler/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from django_celery_beat.models import PeriodicTask, IntervalSchedule
+
 from .models import Endpoint, CheckLog
 
 
@@ -8,6 +10,17 @@ from .models import Endpoint, CheckLog
 def create_endpoint(sender, instance, created, **kwargs):
     if created:
         print("ENDPOINT IS CREATED - SIGNAL!")
+
+        interval, _ = IntervalSchedule.objects.get_or_create(
+            every=30,
+            period=IntervalSchedule.SECONDS,
+        )
+
+        PeriodicTask.objects.create(
+            interval=interval,
+            name="endpoint-schedule",
+            task="scheduler.tasks.endpoint_checker",
+        )
 
 
 @receiver(post_save, sender=CheckLog)
