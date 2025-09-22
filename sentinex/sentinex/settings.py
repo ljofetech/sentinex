9f8237ce956a5596c60021e3b20ae8d05fb26138
StudentFPW <jofeleonids00@gmail.com>
2025-09-22 19:02:36 +0300

    added docker compose

--- sentinex/sentinex/settings.py
+++ sentinex/sentinex/settings.py
@@ -170,6 +170,6 @@ SPECTACULAR_SETTINGS = {
 }
 
 # Celery Configuration Options
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
 CELERY_TIMEZONE = "UTC"
